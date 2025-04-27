bba0a976c6f956f39d7266767c385c90f1844ffd
carole27888 <cmutemi27@gmail.com>
2025-04-27 17:24:47 +0300

    initial commit

--- src/components/ExperienceSection/ExperienceTopLeft.jsx
+++ src/components/ExperienceSection/ExperienceTopLeft.jsx
@@ -6,7 +6,7 @@ const ExperienceTopLeft = () => {
     <div className='flex flex-col gap-6 w-[300px]'>
         <p className='text-orange font-bold uppercase text-3xl font-special text-center'>Since 2023</p>
         <div className='flex justify-center items-center gap-4'>
-            <ExperienceInfo number="1" text="Year" />
+            <ExperienceInfo number="2.5" text="Year" />
             <p className='font-bold text-6xl text-white'>-</p>
             <ExperienceInfo number="5" text="Websites"/>
         </div>
