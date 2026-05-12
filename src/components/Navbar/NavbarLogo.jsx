d8aaa6183c2c6b4403d397b40aa9940d55fa441c
Carole Mutemi <cmutemi27@gmail.com>
2026-05-12 11:01:08 +0300

    changed the title

--- src/components/Navbar/NavbarLogo.jsx
+++ src/components/Navbar/NavbarLogo.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className='hidden md:block text-white text-2xl'>
-        Executive Assistant
-      </h1>
-      <h1 className="block md:hidden text-white font-special font-extrabold text-3xl sm:text-4xl">
-        EA
-      </h1>
+      <img src="/images/LOGO.jpeg" alt="Logo" className="h-10 w-auto object-contain" />
     </div>
   )
 }
